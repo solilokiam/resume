374a5bebe32a32cbbf062d035f219141a7cb864b
Miquel Company Rodriguez <miquel@kodify.io>
2021-08-20 17:35:03 +0200

    Support vercel pdf generation using chrome-aws-lambda

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -1,13 +1,21 @@
-import puppeteer from 'puppeteer';
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 export default async function handler(req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/pdf', 'Content-Disposition': 'attachment;filename="MiquelCompanyRodriguez.pdf"' });
-	const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  } : {
+    args: [],
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  });
+
 	const page = await browser.newPage();
 	await page.goto('https://resume.solilokiam.com');
 	const buffer = await page.pdf({ printBackground: true, width: '1400px', height: '2200px' });
-
 	await browser.close();
 
+  res.writeHead(200, { 'Content-Type': 'application/pdf', 'Content-Disposition': 'attachment;filename="MiquelCompanyRodriguez.pdf"' });
   res.end(buffer);
 }

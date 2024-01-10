b9dddd985a168456c40e3d2bfe776464f4ffb6bf
Miquel Company Rodriguez <miquel.c@landbot.io>
2024-01-10 13:50:03 +0100

    create a pili version

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -1,21 +1,37 @@
-import chrome from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  } : {
-    args: [],
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  const browser = await puppeteer.launch(
+    process.env.AWS_EXECUTION_ENV
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath:
+            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        }
+  );
+
+  const page = await browser.newPage();
+  await page.goto(
+    process.env.AWS_EXECUTION_ENV
+      ? "https://resume.solilokiam.com?eagerMode=1"
+      : "http://localhost:3000?eagerMode=1"
+  );
+  const buffer = await page.pdf({
+    printBackground: true,
+    width: "1400px",
+    height: "2200px",
   });
+  await browser.close();
 
-	const page = await browser.newPage();
-	await page.goto(process.env.AWS_EXECUTION_ENV ? 'https://resume.solilokiam.com?eagerMode=1': 'http://localhost:3000?eagerMode=1');
-	const buffer = await page.pdf({ printBackground: true, width: '1400px', height: '2200px' });
-	await browser.close();
-
-  res.writeHead(200, { 'Content-Type': 'application/pdf', 'Content-Disposition': 'attachment;filename="MiquelCompanyRodriguez.pdf"' });
+  res.writeHead(200, {
+    "Content-Type": "application/pdf",
+    "Content-Disposition": 'attachment;filename="PilarGraciaMurillo.pdf"',
+  });
   res.end(buffer);
 }
